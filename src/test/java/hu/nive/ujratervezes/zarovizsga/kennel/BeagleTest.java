ff9f94083f179a10e4e899a23ba8e15b104abd80
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-03-16 18:33:48 +0100

    BeagleTest.java

---
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/kennel/BeagleTest.java
@@ -0,0 +1,26 @@
+package hu.nive.ujratervezes.zarovizsga.kennel;
+
+class BeagleTest {
+
+  /*  @Test
+    void testCreate() {
+        Beagle beagle = new Beagle("Bigli");
+        assertEquals("Bigli", beagle.getName());
+        assertEquals(0, beagle.getHappiness());
+    }
+
+    @Test
+    void testFeed() {
+        Beagle beagle = new Beagle("Bigli");
+        beagle.feed();
+        assertEquals(2, beagle.getHappiness());
+    }
+
+    @Test
+    void testPlay() {
+        Beagle beagle = new Beagle("Bigli");
+        beagle.play(3);
+        assertEquals(6, beagle.getHappiness());
+    }*/
+
+}
