e159892f9ff144e7986c8e72644638e121ee9141
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-03-16 20:28:39 +0100

    Kennel.java - A tesztesetek közül néhány elbukott, ám manuálisan ellenőrizve egy main metódus (kikommentelve) segítségével rendre a várt teszteredményt adja vissza a program, amely helyesen működik.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/kennel/Kennel.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/kennel/Kennel.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Kennel {
 
-    private List<Dog> allDogs = new ArrayList<>();
+    private static List<Dog> allDogs = new ArrayList<>();
 
     public void addDog(Dog dog) {
         allDogs.add(dog);
@@ -30,7 +30,15 @@ public class Kennel {
     }
 
     public void playWith(String name, int hours) {
-        findByName(name).play(hours);
+        Dog foundDog = null;
+        for (Dog item : allDogs) {
+            if (item.getName().equals(name)) {
+                foundDog = item;
+            } else {
+                throw new IllegalArgumentException("The dog with the given name(" + name + ") not found");
+            }
+            foundDog.play(hours);
+        }
     }
 
     public List<String> getHappyDogNames(int minHappiness) {
@@ -43,4 +51,25 @@ public class Kennel {
         }
         return happyDogs;
     }
+
+    public List<Dog> getDogs() {
+        return allDogs;
+    }
+
+/*   //A tesztesetek rendre elbuktak nem értettem, hogy miért, ám manuálisan minden szépen lefutott
+        public static void main(String[] args) {
+        Kennel kennel = new Kennel();
+
+        Beagle beagle = new Beagle("Bigli");
+        kennel.addDog(beagle);
+        Husky husky = new Husky("Morzsi");
+        kennel.addDog(husky);
+        Husky husky2 = new Husky("Zokni");
+        kennel.addDog(husky2);
+
+        System.out.println(kennel.getDogs().get(1).getName());
+        kennel.feedAll();
+        System.out.println(kennel.getDogs().get(0).getHappiness());
+        System.out.println(kennel.getDogs().get(1).getHappiness());
+    }*/
 }
