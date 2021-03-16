2b047436ee66e3e96c478e786c2c812d61a63dc9
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-03-16 18:34:21 +0100

    HuskyTest.java

---
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/kennel/HuskyTest.java
@@ -0,0 +1,25 @@
+package hu.nive.ujratervezes.zarovizsga.kennel;
+
+public class HuskyTest {
+
+ /*   @Test
+    void testCreate() {
+        Husky husky = new Husky("Morzsi");
+        assertEquals("Morzsi", husky.getName());
+        assertEquals(0, husky.getHappiness());
+    }
+
+    @Test
+    void testFeed() {
+        Husky husky = new Husky("Morzsi");
+        husky.feed();
+        assertEquals(4, husky.getHappiness());
+    }
+
+    @Test
+    void testPlay() {
+        Husky husky = new Husky("Morzsi");
+        husky.play(4);
+        assertEquals(12, husky.getHappiness());
+    }*/
+}
